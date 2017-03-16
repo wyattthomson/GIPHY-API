e4005521af33427bd81705d115ac8534e27ac0e2
Wyatt Thomson <wyattthomson@Wyatts-MacBook-Pro.local>
2017-03-15 18:06:07 -0700

    basic outline with jQuery still in HTML

---
+++ giphy.js
@@ -0,0 +1,11 @@
+$(".gif").on("click", function() {
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
